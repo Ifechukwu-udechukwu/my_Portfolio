685b6b6a4bae58b95642d845a9db065101262429
ifechukwu-udechukwu <peterifechukwuudechukwu@gmail.com>
2025-10-23 13:41:39 +0100

    updated navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,10 @@
 import { assets } from "@/assets/assets"
 import Image from "next/image"
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 const Navbar = () => {
+
+    const [isScroll, setIsScroll] = useState(false)
 
     const sideMenuRef = useRef();
 
@@ -14,17 +16,27 @@ const Navbar = () => {
         sideMenuRef.current.style.transform = "translateX(16rem)"
     }
 
+    useEffect(()=>{
+        window.addEventListener("scroll", ()=>{
+            if (scrollY > 50) {
+                setIsScroll(true)
+            }else{
+                setIsScroll(false)
+            }
+        })
+    },[])
+
     return (
         <>
             <div className="fixed top-0 right-0 -z-10 translate-y-[-80%]">
                 <Image src={assets.header_bg_color} alt="icon" className="w-full" />
             </div>
-            <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
+            <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""}`}>
                 <a href="#top" className="text-3xl font-outfit font-bold tracking-tight text-gray-900">
                     Ife<span className="text-pink-600">Chukwu.</span>
                 </a>
 
-                <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white/50 shadow-sm ">
+                <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white/100 shadow-sm"} `}>
                     <li><a className="font-ovo" href="#top">Home</a></li>
                     <li><a className="font-ovo" href="#about">About me</a></li>
                     <li><a className="font-ovo" href="#services">Services</a></li>
